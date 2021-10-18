8d356279b4b595528a4a2e93a0c3252dff72e4be
emacc99 <lalitoceja@gmail.com>
2021-10-18 17:48:05 -0500

    more string comparitions

--- src/manejadores/manejador_S.cpp
+++ src/manejadores/manejador_S.cpp
@@ -1,7 +1,7 @@
 #include "../../include/manejadores/manejador_S.hpp"
 #include "../../include/manejadores/mensajes.hpp"
 
-bool string_checker(const std::string &a, const std::string &b, const size_t size){
+bool bad_string_checker(const std::string &a, const std::string &b, const size_t size){
     //esto es adrede
     for (int i = 0; i < size; ++i){
         if (a[i] != b[i])
@@ -10,6 +10,9 @@ bool string_checker(const std::string &a, const std::string &b, const size_t siz
     return true;
 }
 
+bool not_so_bad_string_checker(const std::string &a, const std::string&b, const size_t size){
+    return a == b ? true : false;
+}
 
 bool manejador_servidor(int sockfd, const std::string &correct_pass){
     std::string mensaje;
@@ -18,7 +21,7 @@ bool manejador_servidor(int sockfd, const std::string &correct_pass){
     if (mensaje == "exit")
         return false;
     
-    if (string_checker(mensaje, correct_pass, correct_pass.size()))
+    if (not_so_bad_string_checker(mensaje, correct_pass, correct_pass.size()))
         enviar_mensaje(sockfd, "logeado");
     
     else
