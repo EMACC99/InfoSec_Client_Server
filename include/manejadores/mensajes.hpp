15b9602d73a8f7134548fe2c42e448a13993038b
emacc99 <lalitoceja@gmail.com>
2021-12-12 23:08:19 -0600

    send and receive data for win client version using native winsocket functions

--- include/manejadores/mensajes.hpp
+++ include/manejadores/mensajes.hpp
@@ -2,7 +2,15 @@
 
 #include "../sockets/socket_includes.hpp"
 
-void recibir_mensaje(int sockfd, std::string &mensaje);
 
+#ifdef _WIN32
+void recibir_mensaje(SOCKET s, std::string &message);
+void enviar_mensaje(SOCKET s, std::string& message);
+void enviar_mensaje(SOCKET s, const char* message);
+
+#elif __linux__
+
+void recibir_mensaje(int sockfd, std::string &mensaje);
 void enviar_mensaje(int sockfd, std::string &mensaje);
 void enviar_mensaje(int sockfd, const char *mensaje);
+#endif // __WIN32
--- src/manejadores/mensajes.cpp
+++ src/manejadores/mensajes.cpp
@@ -1,6 +1,35 @@
 #include "../../include/manejadores/mensajes.hpp"
 
-void recibir_mensaje(int sockfd, std::string &mensaje){
+
+
+#ifdef _WIN32
+void recibir_mensaje(SOCKET s, std::string& message) {
+    char buff[MAX];
+    bzero(buff, MAX);
+    recv(s, buff, sizeof(buff), 0);
+    message = buff;
+}
+
+void enviar_mensaje(SOCKET s, std::string& message) {
+
+    char buff[MAX];
+    bzero(buff, MAX);
+
+    const char* x = message.c_str();
+    send(s, x, sizeof(buff), 0);
+} 
+
+void enviar_mensaje(SOCKET s, const char* message) {
+    char buff[MAX];
+    bzero(buff, MAX);
+
+    send(s, message, sizeof(buff), 0);
+}
+
+
+
+#elif __linux__
+void recibir_mensaje(int sockfd, std::string& mensaje) {
     char buff[MAX];
     bzero(buff, MAX);
     read(sockfd, buff, sizeof(buff));
@@ -20,3 +49,4 @@ void enviar_mensaje(int sockfd, const char *mensaje){
     bzero(buff, MAX);
     write(sockfd, mensaje, sizeof(buff));
 }
+#endif
